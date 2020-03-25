17f39ee5836113bf66cf104952fd852ad2540cb4
sylor_huang@126.com <sylor_huang@126.com>
2020-03-25 18:44:07 +0800

    修改数量

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -42,7 +42,7 @@ class ProjectsController < ApplicationController
     if is_admin
       projects = Project.all
     elsif current_user&.logged?
-      projects = Project.joins(:members).where.not("projects.is_public = ? and (projects.user_id != ? or members.user_id != ?)", false, current_user.id,current_user.id )
+      projects = Project.joins(:members).where.not("projects.is_public = ? and (projects.user_id != ? or members.user_id != ?)", false, current_user.id,current_user.id ).distinct
     else
       projects = Project.visible
     end
