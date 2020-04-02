546e112f808a05287b92974f65f929a74c36004f
Jasder <2053003901@@qq.com>
2020-04-02 14:33:13 +0800

    FIX 解决后台管理导航的问题

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -4,7 +4,7 @@ class Admins::UsersController < Admins::BaseController
     params[:sort_direction] = params[:sort_direction].presence || 'desc'
 
     users = Admins::UserQuery.call(params)
-    @users = paginate users.includes(user_extension: :school)
+    @users = paginate users.includes(:user_extension)
   end
 
   def edit
