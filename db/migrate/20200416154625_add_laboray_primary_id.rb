8a045f0100779ebae9ddccb7ec101e36c4ccfbbf
sylor_huang@126.com <sylor_huang@126.com>
2020-04-17 00:02:40 +0800

    修改主域名的跳转

--- db/migrate/20200416154625_add_laboray_primary_id.rb
+++ db/migrate/20200416154625_add_laboray_primary_id.rb
@@ -1,5 +1,5 @@
 class AddLaborayPrimaryId < ActiveRecord::Migration[5.2]
   def change
-    execute "ALTER TABLE laboratories ADD PRIMARY KEY (id);"
+    # execute "ALTER TABLE laboratories ADD PRIMARY KEY (id);"
   end
 end
