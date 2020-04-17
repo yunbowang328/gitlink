8eb33338a8d7ec07f2579ea041db4de2480d5eb8
sylor_huang@126.com <sylor_huang@126.com>
2020-04-17 17:58:56 +0800

    修改我的项目的链接

--- db/migrate/20200417094626_add_laboratory_setting_id_index.rb
+++ db/migrate/20200417094626_add_laboratory_setting_id_index.rb
@@ -1,5 +1,5 @@
 class AddLaboratorySettingIdIndex < ActiveRecord::Migration[5.2]
   def change
-    execute "ALTER TABLE laboratory_settings ADD PRIMARY KEY (id);"
+    # execute "ALTER TABLE laboratory_settings ADD PRIMARY KEY (id);"
   end
 end
