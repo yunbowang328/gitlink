ac66a618923b08c356c553fb384a3882fb0c1a22
sylor_huang@126.com <sylor_huang@126.com>
2020-04-17 10:42:34 +0800

    修改user_projects的链接

--- app/controllers/concerns/laboratory_helper.rb
+++ app/controllers/concerns/laboratory_helper.rb
@@ -37,7 +37,7 @@ module LaboratoryHelper
       new_course: "https://www.trustie.net/courses/new",
       edit_account: "https://www.trustie.net/my/account",
       my_courses: "https://www.trustie.net/users/#{current_user.try(:login)}/user_courselist",
-      my_projects: "https://www.trustie.net/users/#{current_user.try(:login)}/user_projectlist",
+      my_projects: "/users/#{current_user.try(:login)}/projects",
       my_organ: "https://www.trustie.net/users/#{current_user.try(:login)}/user_organizations",
       default_url: "https://www.trustie.net/",
       tiding_url: "https://www.trustie.net/users/#{current_user.try(:login)}/user_messages"
