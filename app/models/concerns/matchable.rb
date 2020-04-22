7142e882566a5fac24338f192664acf317786e43
sylor_huang@126.com <sylor_huang@126.com>
2020-04-22 11:58:36 +0800

    修改bug

--- app/models/concerns/matchable.rb
+++ app/models/concerns/matchable.rb
@@ -6,7 +6,7 @@ module Matchable
     #   joins(:repository).where(%w[ projects.name projects.identifier repositories.identifier ].map { |f| "LOWER(#{f}) LIKE :q" }.join(' OR '), q: "%#{keywords.split(" ").join('|')}%") unless keywords.blank?
     # }
     scope :like, lambda { |keywords|
-      joins(:repository).where("LOWER(projects.name) like ? or LOWER(repositories.identifier) like ?", "%#{keywords.downcase}","%#{keywords.downcase}") unless keywords.blank?
+      joins(:repository).where("LOWER(projects.name) like ? or LOWER(projects.identifier) like ? or LOWER(repositories.identifier) like ?", "%#{keywords.downcase}","%#{keywords.downcase}","%#{keywords.downcase}") unless keywords.blank?
     }
     scope :with_project_category, ->(category_id) { where(project_category_id: category_id) unless category_id.blank? }
     scope :with_project_language, ->(language_id) { where(project_language_id: language_id) unless language_id.blank? }
