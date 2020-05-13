96e03ac249c7ab2a031c7d93e2ca1b849b1e9fa9
sylor_huang@126.com <sylor_huang@126.com>
2020-05-13 17:15:22 +0800

    修改已发行版本的数量问题

--- app/models/version_release.rb
+++ app/models/version_release.rb
@@ -2,6 +2,6 @@ class VersionRelease < ApplicationRecord
   belongs_to :repository, counter_cache: true
   belongs_to :user
   has_many :project_trends, as: :trend, dependent: :destroy
-  scope :releases_size, ->{where(draft: false, prerelease: false)}
+  scope :releases_size, ->{where(draft: false, prerelease: false).size}
   # has_many :attachments, as: :container, dependent: :destroy
 end
