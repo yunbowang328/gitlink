29b8a92b9aa0a3ad9e2a2f6f6b08beed98b87282
sylor_huang@126.com <sylor_huang@126.com>
2020-05-18 17:39:36 +0800

    修改迁移文件2

--- db/migrate/20200518092612_change_project_watchers_count.rb
+++ db/migrate/20200518092612_change_project_watchers_count.rb
@@ -4,7 +4,7 @@ class ChangeProjectWatchersCount < ActiveRecord::Migration[5.2]
     watcher_ids = Watcher.where(watchable_type: "Project").pluck(:watchable_id).uniq 
     watcher_ids.each do |i|
       puts "#######____update_project_watchers_id____##############{i}"
-      p = Project.includes(:watchers).select(:id, :watchers_count).find_by(i) 
+      p = Project.includes(:watchers).select(:id, :watchers_count).find_by(id:i) 
       if p.present?
         Project.reset_counters(i, :watchers)
       end
