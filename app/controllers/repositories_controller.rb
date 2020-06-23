4facaf7076e94b89bb33c414a665604f0f05bc97
sylor_huang@126.com <sylor_huang@126.com>
2020-06-23 14:56:53 +0800

    change repository images error

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -32,6 +32,7 @@ class RepositoriesController < ApplicationController
     @project_owner = @project.owner
     @entries = Gitea::Repository::Entries::ListService.new(@project_owner, @project.identifier, ref: @ref).call
     @entries = @entries.sort_by{ |hash| hash['type'] }
+    @path = Gitea.gitea_config[:domain]+"/#{@project.owner.login}/#{@project.identifier}/raw/branch/#{@ref}/"
   end
 
   def sub_entries
