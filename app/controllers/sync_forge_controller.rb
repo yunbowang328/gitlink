e9f93ddc20fd17bd13cad4c25264d1ef9e676204
sylor_huang@126.com <sylor_huang@126.com>
2020-07-10 15:35:51 +0800

    change project_score

--- app/controllers/sync_forge_controller.rb
+++ app/controllers/sync_forge_controller.rb
@@ -24,9 +24,10 @@ class SyncForgeController < ApplicationController
         Rails.logger.info("=================new_repository_id:#{project&.repository&.id}========")
         if project.present?
           if sync_params[:project_score].present?
-            score_params = sync_params[:project_score].merge(project_id: project.id)
-            Rails.logger.info("=================new_score_params:#{score_params}========")
-            new_project_score = ProjectScore.create!(score_params)
+            score_params = sync_params[:project_score]
+            new_project_score = ProjectScore.create(:project_id => project.id)
+            Rails.logger.info("=================new_project_score:#{new_project_score}========")
+            new_project_score.update_attributes(score_params)
             Rails.logger.info("=================new_project_score:#{new_project_score}========")
           end
           SyncRepositoryJob.perform_later(project.repository, sync_params[:repository]) if sync_params[:repository].present?
