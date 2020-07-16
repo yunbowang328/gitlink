5cd9fe9e341ad0eddd10cd1af30d3b098ec1d92c
sylor_huang@126.com <sylor_huang@126.com>
2020-07-16 14:25:30 +0800

    change

--- db/migrate/20200716060536_change_projects_default_count.rb
+++ db/migrate/20200716060536_change_projects_default_count.rb
@@ -3,7 +3,7 @@ class ChangeProjectsDefaultCount < ActiveRecord::Migration[5.2]
     projects = Project.select(:id, :issues_count,:pull_requests_count,:versions_count,:praises_count,:watchers_count).all
     projects.each do |p|
       puts p.id
-      Project.reset_counters( p.id, :issues_count, touch: false )
+      # Project.reset_counters( p.id, :issues_count, touch: false )
       Project.reset_counters( p.id, :pull_requests_count, touch: false )
       Project.reset_counters( p.id, :versions_count, touch: false )
       # Project.reset_counters( p.id, :praises_count, touch: false )
