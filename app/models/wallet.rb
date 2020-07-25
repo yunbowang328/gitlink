0f692554226e77f03f841d8d75f12bd34703a52b
wyx <414722560@qq.com>
2020-07-25 12:12:34 +0800

    update model wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,2 +1,6 @@
 class Wallet < ApplicationRecord
+  belongs_to :user
+  has_many :outcome, class_name: 'CoinChange', foreign_key: 'from_wallet_id', dependent: :destroy
+  has_many :income, class_name: 'CoinChange', foreign_key: 'to_wallet_id', dependent: :destroy
+  validates :balance, presence: true
 end
