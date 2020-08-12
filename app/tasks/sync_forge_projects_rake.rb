645e4a35866e72e9e3c1ae28b41979d97a36437e
sylor_huang@126.com <sylor_huang@126.com>
2020-08-12 11:45:57 +0800

    Change

--- app/tasks/sync_forge_projects_rake.rb
+++ app/tasks/sync_forge_projects_rake.rb
@@ -1,7 +1,11 @@
+require 'uri'
+require 'net/http'
+
 class SyncForgeProjectsRake
   # 运行示例: 检查哪些项目的repo不存在，bundle exec rails runner "SyncForgeProjectsRake.new.call()"
   
     def call
       
+
     end
   end
