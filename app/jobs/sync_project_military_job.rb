3a0ec56564ca788c259c92de48b46f552819e3a7
sylor_huang@126.com <sylor_huang@126.com>
2020-08-12 11:12:04 +0800

    Chnage

--- app/jobs/sync_project_military_job.rb
+++ app/jobs/sync_project_military_job.rb
@@ -12,7 +12,7 @@ class SyncProjectMilitaryJob < ApplicationJob
       repository: repository.as_json, 
       project_socre: project_socre.as_json
     }
-    url = "http://47.93.212.82:49999/sync_forges"  #trustie上的相关路由
+    url = "http://47.93.212.82:49999/sync_forge/sync_projects"  #trustie上的相关路由
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.hostname, uri.port)
     http.use_ssl = false
