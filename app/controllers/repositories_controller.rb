c56c98fd6e013c94ac80705e97aa9636ef030250
sylor_huang@126.com <sylor_huang@126.com>
2020-08-12 15:30:39 +0800

    Change

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -102,9 +102,9 @@ class RepositoriesController < ApplicationController
   end
 
   def sync_mirror
-    return render_error("正在镜像中..") if  @repo.mirror.waiting?
+    return render_error("正在镜像中..") if  @repo&.mirror&.waiting?
 
-    @repo.sync_mirror!
+    @repo.sync_mirror! if @repo&.mirror
     SyncMirroredRepositoryJob.perform_later(@repo.id, current_user.id)
     render_ok
   end
