193ba325ce2b3260da57de8c9b4bd526ac9fd156
Jasder <2053003901@@qq.com>
2020-08-12 22:24:06 +0800

    FIX code bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   include ApplicationHelper
   include OperateProjectAbilityAble
   include ProjectsHelper
-  before_action :require_login, except: %i[index branches group_type_list simple, show]
+  before_action :require_login, except: %i[index branches group_type_list simple show]
   before_action :load_project, except: %i[index group_type_list migrate]
   before_action :authorizate_user_can_edit_project!, only: %i[update]
   before_action :project_public?, only: %i[fork_users praise_users watch_users]
