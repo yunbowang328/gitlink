9b25b7a9e6d83309ecf7557ddac122ced16fb2e7
Jasder <2053003901@@qq.com>
2020-08-18 16:40:39 +0800

    FIX ci drone server database

--- app/libs/ci/drone/server.rb
+++ app/libs/ci/drone/server.rb
@@ -19,7 +19,7 @@ class Ci::Drone::Server
     "service docker start; docker rm -f `docker ps -qa`; docker run \
       -v /var/run/docker.sock:/var/run/docker.sock \
       -e DRONE_DATABASE_DRIVER=mysql \
-      -e DRONE_DATABASE_DATASOURCE=#{database_username}:#{database_password}@tcp\(#{database_host}:#{database_port}\)/drone?parseTime=true \
+      -e DRONE_DATABASE_DATASOURCE=#{database_username}:#{database_password}@tcp\\(#{database_host}:#{database_port}\\)/drone?parseTime=true \
       -e DRONE_GITEA_SERVER=#{gitea_url} \
       -e DRONE_GITEA_CLIENT_ID=#{client_id} \
       -e DRONE_GITEA_CLIENT_SECRET=#{client_secret} \
