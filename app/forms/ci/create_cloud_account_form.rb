72feacecbb1e66b970988ad91d57b837c213c0d8
sylor_huang@126.com <sylor_huang@126.com>
2020-08-24 14:12:59 +0800

    Test Project Validate Failed

--- app/forms/ci/create_cloud_account_form.rb
+++ app/forms/ci/create_cloud_account_form.rb
@@ -3,7 +3,8 @@ class Ci::CreateCloudAccountForm
 
   attr_accessor :project_id, :ip_num, :account, :secret
 
-  validates :project_id, :account, :secret, presence: true
+  # validates :project_id, :account, :secret, presence: true
+  validates :account, :secret, presence: true
   validates :ip_num, presence: true, format: { with: CustomRegexp::IP, multiline: true, message: 'IP 地址格式不对' }
 
 end
