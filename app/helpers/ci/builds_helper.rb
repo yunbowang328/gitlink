3e25a7b28cbf3e76511479d6ca99fe1f39c75fa2
Jasder <2053003901@@qq.com>
2020-08-25 15:49:47 +0800

    FIX code review

--- app/helpers/ci/builds_helper.rb
+++ app/helpers/ci/builds_helper.rb
@@ -1,6 +1,5 @@
 module Ci::BuildsHelper
   def format_utc_time(unix_time)
-    Rails.logger.info "00000000000000 #{unix_time}"
     return nil if unix_time == 0
     DateTime.strptime(unix_time.to_s,'%s').strftime("%Y-%m-%d %H:%M")
   end
