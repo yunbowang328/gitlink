b26c2f273f7a9550f8112ecbdad00e39a0674937
Jasder <2053003901@@qq.com>
2020-08-27 16:01:10 +0800

    FIX build time bug

--- app/helpers/ci/builds_helper.rb
+++ app/helpers/ci/builds_helper.rb
@@ -1,7 +1,7 @@
 module Ci::BuildsHelper
   def format_utc_time(unix_time)
     return nil if unix_time == 0
-    DateTime.strptime(unix_time.to_s,'%s').strftime("%Y-%m-%d %H:%M")
+    Time.at(unix_time).strftime("%Y-%m-%d %H:%M")
   end
 
   def render_duartion_time(end_time, start_time)
