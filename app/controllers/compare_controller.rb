5667c0854c3bd25174125c956a6366af04d49d38
Jasder <2053003901@@qq.com>
2021-01-12 17:34:07 +0800

    FIX [api] get pull bug
    (cherry picked from commit fa01194d3c013112b6548abc20bf71c7d14b2609)

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -6,10 +6,26 @@ class CompareController < ApplicationController
   end
 
   def show
+    compare
+  end
+
+  private
+  def compare
+    base, head = compare_params
+
+    # TODO: 处理fork的项目向源项目发送PR的base、head参数问题
+    @compare_result ||=
+      head.include?(":") ? gitea_compare(base, head) : gitea_compare(head, base)
+  end
+
+  def compare_params
     base = Addressable::URI.unescape(params[:base])
     head = params[:head].include?('json') ? params[:head]&.split('.json')[0] : params[:head]
-    @compare_result = Gitea::Repository::Commits::CompareService.call(@owner.login, @project.identifier, head, base)
 
-    # render json: @compare_result
+    [base, head]
+  end
+
+  def gitea_compare(base, head)
+    Gitea::Repository::Commits::CompareService.call(@owner.login, @project.identifier, base, head)
   end
 end
