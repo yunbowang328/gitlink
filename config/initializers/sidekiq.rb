1e9bd451e59ab2589cce12f18fc99c40b0929ead
Jasder <2053003901@@qq.com>
2021-01-14 18:22:01 +0800

    FIX updage sidekiq cron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 redis_config = Rails.application.config_for(:redis)
-sidekiq_url = redis_config["url"]
+sidekiq_url = redis_config["url"] || 'redis://localhost:6379/0'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: sidekiq_url }
 
-  schedule_file = "config/schedule.yml"
+  schedule_file = "config/sidekiq_cron.yml"
   if File.exists?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   end
