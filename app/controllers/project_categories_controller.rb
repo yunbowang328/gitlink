ce105b4c07e76c3566bb4065dc735278fc56d639
vilet.yy <yystopf@163.com>
2021-02-26 16:20:26 +0800

    [FIX]project category not include private repo

--- app/controllers/project_categories_controller.rb
+++ app/controllers/project_categories_controller.rb
@@ -6,6 +6,8 @@ class ProjectCategoriesController < ApplicationController
   end
 
   def group_list
-    @project_categories = ProjectCategory.where('projects_count > 0').order(projects_count: :desc)
+    # @project_categories = ProjectCategory.where('projects_count > 0').order(projects_count: :desc)
+    @project_categories = ProjectCategory.eager_load(:projects)
+        .where(projects: {is_public: true})
   end
 end
--- app/views/project_categories/group_list.json.jbuilder
+++ app/views/project_categories/group_list.json.jbuilder
@@ -1,5 +1,6 @@
 json.array! @project_categories do |category|
   json.id category.id
   json.name category.name
-  json.projects_count category.projects_count
+  # json.projects_count category.projects_count
+  json.projects_count category.projects.size
 end
