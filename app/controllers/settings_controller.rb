815b3644281081db3238cf201a32938586a6cce3
jasder <jasduaner@gmail.com>
2021-03-23 15:42:56 +0800

    ADD common url with setting api

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,7 +3,7 @@ class SettingsController < ApplicationController
     @old_projects_url = nil
     @old_projects_url = "https://www.trustie.net/users/#{current_user.try(:login)}/projects"  if User.current.logged?
 
-    @add =  Site.add
+    @add =  Site.add.select(:id, :name, :url, :key)
     @personal =
       if User.current.logged?
         arr =[]
@@ -17,6 +17,14 @@ class SettingsController < ApplicationController
       else
         []
       end
-    # @sites = Site.group(:site_type).select(:name, :url, :key)
+
+    @common = []
+    Site.common.select(:id, :name, :url, :key).to_a.map(&:serializable_hash).each do |site|
+      hash = {}
+      site.each {|k, v|
+        hash.merge!("#{k}":  v.to_s.include?("current_user") ? v.split('current_user').join(current_user&.login) : v)
+      }
+      @common << hash
+    end
   end
 end
