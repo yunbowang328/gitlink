dc91cd43461bd856a653b79d2a6f5abe7aa94ef4
vilet.yy <yystopf@163.com>
2021-05-31 18:46:39 +0800

    fix

--- db/migrate/20210531100250_add_fields_to_user_extensions.rb
+++ db/migrate/20210531100250_add_fields_to_user_extensions.rb
@@ -3,6 +3,6 @@ class AddFieldsToUserExtensions < ActiveRecord::Migration[5.2]
     add_column :user_extensions, :province, :string # 省份
     add_column :user_extensions, :city, :string   # 城市
     add_column :user_extensions, :custom_department, :string  #自己填写的单位名称
-    remove_column :users, :description
+    # remove_column :users, :description
   end
 end
