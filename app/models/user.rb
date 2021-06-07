89bc20e5407d59729289920d41f06a8ffa7f4e71
jasder <jasduaner@gmail.com>
2021-06-07 16:44:17 +0800

    FIX 去掉User模型中的open_users关联

--- app/models/user.rb
+++ app/models/user.rb
@@ -113,7 +113,7 @@ class User < Owner
 
   belongs_to :laboratory, optional: true
   has_one :user_extension, dependent: :destroy
-  has_many :open_users, dependent: :destroy
+  # has_many :open_users, dependent: :destroy
   has_one :wechat_open_user, class_name: 'OpenUsers::Wechat'
   has_one :qq_open_user, class_name: 'OpenUsers::QQ'
   accepts_nested_attributes_for :user_extension, update_only: true
