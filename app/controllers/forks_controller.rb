f9d4316a8e69d735d538cf9c3da651cd4d70af07
yystopf <yystopf@163.com>
2021-11-23 11:43:44 +0800

    fix: change fork project status

--- app/controllers/forks_controller.rb
+++ app/controllers/forks_controller.rb
@@ -13,7 +13,7 @@ class ForksController < ApplicationController
     if current_user&.id == @project.user_id
       render_result(-1, "自己不能fork自己的项目")
     elsif Project.exists?(user_id: current_user.id, identifier: @project.identifier)
-      render_result(-1, "fork失败，你已拥有了这个项目")
+      render_result(0, "fork失败，你已拥有了这个项目")
     end
     # return if current_user != @project.owner
     # render_result(-1, "自己不能fork自己的项目")
