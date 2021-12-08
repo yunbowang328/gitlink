ba9e74f3738e0d1f58b0dcfe2aa2ab16792440dd
xiaoxiaoqiong <xxqfamous@gmail.com>
2021-12-08 15:32:28 +0800

    fixed db:migrate

--- db/migrate/20211115014133_create_claims.rb
+++ db/migrate/20211115014133_create_claims.rb
@@ -1,4 +1,4 @@
-ails db:migrateclass CreateClaims < ActiveRecord::Migration[5.2]
+class CreateClaims < ActiveRecord::Migration[5.2]
   def change
     create_table :claims do |t|
       t.integer :issue_id
