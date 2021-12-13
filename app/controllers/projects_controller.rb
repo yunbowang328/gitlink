1faaf1d7d13b7ee14ca00bc33bed5cf8803d108e
yystopf <yystopf@163.com>
2021-12-13 11:45:58 +0800

    fix: project menu list wiki location

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,9 +17,9 @@ class ProjectsController < ApplicationController
     menu.append(menu_hash_by_name("code")) if @project.has_menu_permission("code")
     menu.append(menu_hash_by_name("issues")) if @project.has_menu_permission("issues")
     menu.append(menu_hash_by_name("pulls")) if @project.has_menu_permission("pulls") && @project.forge?
-    menu.append(menu_hash_by_name("wiki")) if @project.has_menu_permission("wiki") && @project.forge?
     menu.append(menu_hash_by_name("devops")) if @project.has_menu_permission("devops") && @project.forge?
     menu.append(menu_hash_by_name("versions")) if @project.has_menu_permission("versions")
+    menu.append(menu_hash_by_name("wiki")) if @project.has_menu_permission("wiki") && @project.forge?
     menu.append(menu_hash_by_name("resources")) if @project.has_menu_permission("resources") && @project.forge?
     menu.append(menu_hash_by_name("activity"))
     menu.append(menu_hash_by_name("settings")) if (current_user.admin? || @project.manager?(current_user)) && @project.forge?
