d6b66d532459f411002f4449f83b17e3ae801f39
yystopf <yystopf@163.com>
2021-12-29 09:56:49 +0800

    fix

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -43,10 +43,10 @@ class CompareController < ApplicationController
 
   def load_compare_params
     # @base = Addressable::URI.unescape(params[:base])
-    @base = params[:base].include?(":") ? Addressable::URI.unescape(params[:base].split(":")[0]) + Base64.decode64(params[:base].split(":")[1]) : Base64.decode64(params[:base])
+    @base = params[:base].include?(":") ? Addressable::URI.unescape(params[:base].split(":")[0]) + ':' + Base64.decode64(params[:base].split(":")[1]) : Base64.decode64(params[:base])
     @head = params[:head].include?('.json') ? params[:head][0..-6] : params[:head]
     # @head = Addressable::URI.unescape(@head)
-    @head = @head.include?(":") ? Addressable::URI.unescape(@head.split(":")[0]) + Base64.decode64(@head.split(":")[1]) : Base64.decode64(@head)
+    @head = @head.include?(":") ? Addressable::URI.unescape(@head.split(":")[0]) + ':' + Base64.decode64(@head.split(":")[1]) : Base64.decode64(@head)
   end
 
   def gitea_compare(base, head)
